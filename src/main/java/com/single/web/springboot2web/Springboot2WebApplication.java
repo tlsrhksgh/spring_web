2c5ad70992208360b0d7905c495f9875458c41ac
tlsrhksgh <sin99145@naver.com>
2023-02-01 23:48:20 +0900

    프로젝트 생성

---
+++ src/main/java/com/single/web/springboot2web/Springboot2WebApplication.java
@@ -0,0 +1,13 @@
+package com.single.web.springboot2web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2WebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/single/web/springboot2web/Springboot2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.single.web.springboot2web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
