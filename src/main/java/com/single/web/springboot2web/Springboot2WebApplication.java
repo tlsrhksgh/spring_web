67cd2e8afac3d74b865fc7d08138f3b81d9c9f64
tlsrhksgh <sin99145@naver.com>
2023-02-02 19:36:43 +0900

    Feat - 게시글이 생성 및 수정되는 시점에 날짜를 자동으로 업데이트 해주도록 JPA Auditing 기능 추가

--- src/main/java/com/single/web/springboot2web/Springboot2WebApplication.java
+++ src/main/java/com/single/web/springboot2web/Springboot2WebApplication.java
@@ -2,7 +2,9 @@ package com.single.web.springboot2web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Springboot2WebApplication {
 
