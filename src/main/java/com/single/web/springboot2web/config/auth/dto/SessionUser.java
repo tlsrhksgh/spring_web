b8d4520a301ba9133bc1af6b244fa4e729543728
tlsrhksgh <sin99145@naver.com>
2023-02-03 17:58:15 +0900

    Feat - Google Oauth2 인증을 위한 SpringSeucirty 설정 및 index 페이지에 구글 Oauth2 로그인 버튼 추가

---
+++ src/main/java/com/single/web/springboot2web/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.single.web.springboot2web.config.auth.dto;
+
+import com.single.web.springboot2web.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/com/single/web/springboot2web/web/IndexController.java
+++ src/main/java/com/single/web/springboot2web/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.single.web.springboot2web.web;
 
+import com.single.web.springboot2web.config.auth.dto.SessionUser;
 import com.single.web.springboot2web.service.posts.PostsService;
 import com.single.web.springboot2web.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
